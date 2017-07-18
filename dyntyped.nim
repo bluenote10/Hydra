dac6051215086d4803f820223dadf5e3d2541574
Fabian Keller <github.100.fkeller@spamgourmet.com>
2017-07-18 21:11:48 +0200

    cleanup dyntyped a bit

--- dyntyped.nim
+++ dyntyped.nim
@@ -4,48 +4,9 @@ import typetraits
 include "system/inclrtl.nim"
 include "system/hti.nim"
 
-when false:
-  type
-    AnyVal = ref object of RootObj
-      raw: seq[byte]
-      rawTypePtr: pointer
-
-    AnyValRef[T] = ref object of AnyVal
-      x: T
-
-  template rawType(x: AnyVal): PNimType =
-    cast[PNimType](x.rawTypePtr)
-
-  template `rawType=`(x: var AnyVal, p: PNimType) =
-    x.rawTypePtr = cast[pointer](p)
-
-  proc toAnyVal[T](x: var T): AnyVal =
-    var raw = newSeq[byte](sizeOf(T))
-    echo sizeOf(T), raw
-    copyMem(raw[0].addr, x.addr, sizeOf(T))
-    echo sizeOf(T), raw
-    AnyValRef[T](raw: raw, rawTypePtr: nil, x: x) # cast[PNimType](getTypeInfo(x))
-
-  proc to(anyval: AnyVal, T: typedesc): T =
-    copyMem(result.addr, unsafeAddr(anyval.raw[0]), sizeOf(T))
-
-  proc kind(x: AnyVal): AnyKind =
-    result = AnyKind(ord(x.rawType.kind))
-
-
-  proc test(): AnyVal =
-    var s = @[1, 2, 3]
-    result = toAnyVal(s)
-
-  block:
-    for i in 1 .. 100:
-      let a = test()
-      GC_fullCollect()
-      let x = a.to(seq[int])
-      echo x
 
 type
-  AnyVal = ref object of RootObj
+  AnyVal* = ref object of RootObj
     rawTypePtr: PNimType
 
   AnyValRef[T] = ref object of AnyVal
@@ -67,10 +28,11 @@ proc toAnyVal*[T](x: var T): AnyVal =
   AnyValRef[T](rawTypePtr: cast[PNimType](getTypeInfo(x)), x: x)
 
 proc to*(anyval: AnyVal, T: typedesc): T =
-  echo "reconstructing type ", name(T), " of size ", sizeOf(T)
+  # echo "reconstructing type ", name(T), " of size ", sizeOf(T)
   copyMem(result.addr, anyval.getAddr(), sizeOf(T))
 
 proc isType(anyval: AnyVal, T: typedesc): bool =
+  # Type check based on getTypeInfo() => maybe not needed
   var dummy: T
   let expType = cast[PNimType](getTypeInfo(dummy))
   let gvnType = anyval.rawType
@@ -81,8 +43,9 @@ proc isType(anyval: AnyVal, T: typedesc): bool =
   # only a necessary condition, not sufficient
   result = expType.kind == gvnType.kind
 
-
 #[
+# Alternative type check based on method call
+# => deprecated because of "generic method not attachable to object"
 method ofType(a: AnyVal, X: typedesc): bool {.base.} =
   assert false
 
@@ -104,53 +67,66 @@ proc kind*(x: AnyVal): AnyKind =
 
 
 when isMainModule:
-  proc test(): AnyVal =
-    var s = @[1, 2, 3]
-    result = toAnyVal(s)
+
+  template assertNot(x) = assert(not(x))
 
   block:
+    proc constructNested(): AnyVal =
+      var s = @[1, 2, 3]
+      result = toAnyVal(s)
     for i in 1 .. 100:
-      let a = test()
+      let a = constructNested()
       GC_fullCollect()
       let x = a.to(seq[int])
-      echo x
+      assert x == @[1, 2, 3]
 
   block:
-    var x = 1
-    let a = toAnyVal(x)
-    echo a.to(int)
-    echo a.isType(int)
-    echo a.isType(float)
-    echo a.isType(seq[int])
+    proc constructNested(): AnyVal =
+      var x = 1
+      result = toAnyVal(x)
 
-    assert a.ofType(int)
-    assert(not a.ofType(float))
-    assert(not a.ofType(seq[int]))
+    for i in 1 .. 100:
+      let a = constructNested()
+      GC_fullCollect()
+      assert a.to(int) == 1
+      assert a.ofType(int)
+      assertNot a.ofType(float)
+      assertNot a.ofType(seq[int])
 
   block:
     type
       Obj = object
         x: int
         y: int
-    var x = Obj(x: 1, y: 2)
-    let a = toAnyVal(x)
-    echo a.to(Obj)
 
-    echo a.isType(Obj)
-    echo a.isType(int)
-    echo a.isType(float)
-    echo a.isType(seq[int])
+    proc constructNested(): AnyVal =
+      var x = Obj(x: 1, y: 2)
+      result = toAnyVal(x)
+
+    for i in 1 .. 100:
+      let a = constructNested()
+      GC_fullCollect()
+      assert a.to(Obj) == Obj(x: 1, y: 2)
+      assert a.ofType(Obj)
+      assertNot a.ofType(int)
+      assertNot a.ofType(float)
+      assertNot a.ofType(seq[int])
 
   block:
     type
       Obj = ref object
         x: int
         y: int
-    var x = Obj(x: 1, y: 2)
-    let a = toAnyVal(x)
-    echo(a.to(Obj)[])
 
-    echo a.isType(Obj)
-    echo a.isType(int)
-    echo a.isType(float)
-    echo a.isType(seq[int])
+    proc constructNested(): AnyVal =
+      var x = Obj(x: 1, y: 2)
+      result = toAnyVal(x)
+
+    for i in 1 .. 100:
+      let a = constructNested()
+      GC_fullCollect()
+      assert((a.to(Obj)[]) == (Obj(x: 1, y: 2)[]))
+      assert a.ofType(Obj)
+      assertNot a.ofType(int)
+      assertNot a.ofType(float)
+      assertNot a.ofType(seq[int])
